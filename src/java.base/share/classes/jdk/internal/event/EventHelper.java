ibmruntimes/openj9-openjdk-jdk17

8234466: Class loading deadlock involving X509Factory#commitEvent() Reviewed-by: alanb, chegar, dfuchs

src/java.base/share/classes/jdk/internal/event/EventHelper.java
@@ -25,6 +25,11 @@
 
 package jdk.internal.event;
 
+import jdk.internal.access.JavaUtilJarAccess;
+import jdk.internal.access.SharedSecrets;
+
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Date;
@@ -37,14 +42,24 @@ import java.util.stream.IntStream;
 
 public final class EventHelper {
 
+    private static final JavaUtilJarAccess JUJA = SharedSecrets.javaUtilJarAccess();
+    private static volatile boolean loggingSecurity;
+    private static volatile System.Logger securityLogger;
+    private static final VarHandle LOGGER_HANDLE;
+    static {
+        try {
+            LOGGER_HANDLE =
+                    MethodHandles.lookup().findStaticVarHandle(
+                            EventHelper.class, "securityLogger", System.Logger.class);
+        } catch (ReflectiveOperationException e) {
+            throw new Error(e);
+        }
+    }
     private static final System.Logger.Level LOG_LEVEL = System.Logger.Level.DEBUG;
 
     // helper class used for logging security related events for now
     private static final String SECURITY_LOGGER_NAME = "jdk.event.security";
-    private static final System.Logger SECURITY_LOGGER =
-            System.getLogger(SECURITY_LOGGER_NAME);
-    private static final boolean LOGGING_SECURITY =
-            SECURITY_LOGGER.isLoggable(LOG_LEVEL);
+
 
     public static void logTLSHandshakeEvent(Instant start,
                                             String peerHost,
@@ -52,8 +67,9 @@ public final class EventHelper {
                                             String cipherSuite,
                                             String protocolVersion,
                                             long peerCertId) {
+        assert securityLogger != null;
         String prepend = getDurationString(start);
-        SECURITY_LOGGER.log(LOG_LEVEL, prepend +
+        securityLogger.log(LOG_LEVEL, prepend +
         " TLSHandshake: {0}:{1,number,#}, {2}, {3}, {4,number,#}",
         peerHost, peerPort, protocolVersion, cipherSuite, peerCertId);
     }
@@ -61,18 +77,18 @@ public final class EventHelper {
     public static void logSecurityPropertyEvent(String key,
                                                 String value) {
 
-        if (isLoggingSecurity()) {
-            SECURITY_LOGGER.log(LOG_LEVEL,
-                "SecurityPropertyModification: key:{0}, value:{1}", key, value);
-        }
+        assert securityLogger != null;
+        securityLogger.log(LOG_LEVEL,
+            "SecurityPropertyModification: key:{0}, value:{1}", key, value);
     }
 
     public static void logX509ValidationEvent(int anchorCertId,
                                          int[] certIds) {
+        assert securityLogger != null;
         String codes = IntStream.of(certIds)
                 .mapToObj(Integer::toString)
                 .collect(Collectors.joining(", "));
-        SECURITY_LOGGER.log(LOG_LEVEL,
+        securityLogger.log(LOG_LEVEL,
                 "ValidationChain: {0,number,#}, {1}", anchorCertId, codes);
     }
 
@@ -85,7 +101,8 @@ public final class EventHelper {
                                                long certId,
                                                long beginDate,
                                                long endDate) {
-        SECURITY_LOGGER.log(LOG_LEVEL, "X509Certificate: Alg:{0}, Serial:{1}" +
+        assert securityLogger != null;
+        securityLogger.log(LOG_LEVEL, "X509Certificate: Alg:{0}, Serial:{1}" +
             ", Subject:{2}, Issuer:{3}, Key type:{4}, Length:{5,number,#}" +
             ", Cert Id:{6,number,#}, Valid from:{7}, Valid until:{8}",
             algId, serialNum, subject, issuer, keyType, length,
@@ -124,6 +141,14 @@ public final class EventHelper {
      * @return boolean indicating whether an event should be logged
      */
     public static boolean isLoggingSecurity() {
-        return LOGGING_SECURITY;
+        // Avoid a bootstrap issue where the commitEvent attempts to
+        // trigger early loading of System Logger but where
+        // the verification process still has JarFiles locked
+        if (securityLogger == null && !JUJA.isInitializing()) {
+            LOGGER_HANDLE.compareAndSet( null, System.getLogger(SECURITY_LOGGER_NAME));
+            loggingSecurity = securityLogger.isLoggable(LOG_LEVEL);
+        }
+        return loggingSecurity;
     }
+
 }

test/jdk/java/util/jar/JarFile/jarVerification/MultiThreadLoad.java
@@ -1,0 +1,128 @@
+/*
+ * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+import jdk.internal.access.JavaUtilJarAccess;
+import jdk.internal.access.SharedSecrets;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.jar.JarFile;
+
+public class MultiThreadLoad {
+
+    private static PrintStream out = System.err;
+    static String TEST_CLASS_PATH;
+    private static final JavaUtilJarAccess JUJA = SharedSecrets.javaUtilJarAccess();
+    private static CountDownLatch cdl = new CountDownLatch(1);
+
+    private static <T> Set<T> setOf(Iterable<T> it) {
+        Set<T> s = new HashSet<T>();
+        for (T t : it)
+            s.add(t);
+        return s;
+    }
+
+    private static <T> void checkEquals(Set<T> s1, Set<T> s2, boolean eq) {
+        if (s1.equals(s2) != eq)
+            throw new RuntimeException(String.format("%b %s : %s",
+                                                     eq, s1, s2));
+    }
+
+    abstract static class TestLoader {
+        String name;
+
+        TestLoader(String name) { this.name = name; }
+
+        abstract ServiceLoader<FooService> load();
+    }
+
+    static TestLoader tcclLoader = new TestLoader("Thread context class loader") {
+        ServiceLoader<FooService> load() {
+            return ServiceLoader.load(FooService.class);
+        }
+    };
+
+    static TestLoader systemClLoader = new TestLoader("System class loader") {
+        ServiceLoader<FooService> load() {
+            return ServiceLoader.load(FooService.class, ClassLoader.getSystemClassLoader());
+        }
+    };
+
+    static TestLoader nullClLoader = new TestLoader("null (defer to system class loader)") {
+        ServiceLoader<FooService> load() {
+            return ServiceLoader.load(FooService.class, null);
+        }
+    };
+
+    public static void main(String[] args) {
+        // keep reference to variables for the newly launced process
+        TEST_CLASS_PATH = args[0];
+        for (TestLoader tl : Arrays.asList(tcclLoader, systemClLoader, nullClLoader)) {
+            test(tl);
+        }
+    }
+
+    static void test(TestLoader tl) {
+        Runnable r1 = () -> {
+            ServiceLoader<FooService> sl = tl.load();
+            out.format("%s: %s%n", tl.name, sl);
+
+            // Providers are cached
+            Set<FooService> ps = setOf(sl);
+            cdl.countDown();
+            checkEquals(ps, setOf(sl), true);
+
+            // The cache can be flushed and reloaded
+            sl.reload();
+            checkEquals(ps, setOf(sl), false);
+        };
+
+        Runnable r2 = () -> {
+            jarCrawler(Path.of(TEST_CLASS_PATH));
+        };
+        new Thread(r2).start();
+        new Thread(r1).start();
+
+    }
+
+    private static void jarCrawler(Path p) {
+        try {
+            // let the other thread spin up
+            cdl.await();
+        } catch (InterruptedException e) {
+            // ignore
+        }
+        try {
+            for (int i = MultiProviderTest.NUM_JARS -1; i >= 0; i--) {
+                JUJA.ensureInitialization(new JarFile(TEST_CLASS_PATH + File.separator
+                        + "FooProvider" + i + ".jar"));
+            }
+        } catch (Exception e) {
+            System.out.println("Exception during jar crawl: ");
+            e.printStackTrace(System.out);
+        }
+
+    }
+}

vkurdin/JetBrainsRuntime-Shenandoah

src/java.base/share/classes/jdk/internal/event/EventHelper.java
@@ -25,6 +25,11 @@
 
 package jdk.internal.event;
 
+import jdk.internal.misc.JavaUtilJarAccess;
+import jdk.internal.misc.SharedSecrets;
+
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Date;
@@ -37,14 +42,24 @@ import java.util.stream.IntStream;
 
 public final class EventHelper {
 
+    private static final JavaUtilJarAccess JUJA = SharedSecrets.javaUtilJarAccess();
+    private static volatile boolean loggingSecurity;
+    private static volatile System.Logger securityLogger;
+    private static final VarHandle LOGGER_HANDLE;
+    static {
+        try {
+            LOGGER_HANDLE =
+                    MethodHandles.lookup().findStaticVarHandle(
+                            EventHelper.class, "securityLogger", System.Logger.class);
+        } catch (ReflectiveOperationException e) {
+            throw new Error(e);
+        }
+    }
     private static final System.Logger.Level LOG_LEVEL = System.Logger.Level.DEBUG;
 
     // helper class used for logging security related events for now
     private static final String SECURITY_LOGGER_NAME = "jdk.event.security";
-    private static final System.Logger SECURITY_LOGGER =
-            System.getLogger(SECURITY_LOGGER_NAME);
-    private static final boolean LOGGING_SECURITY =
-            SECURITY_LOGGER.isLoggable(LOG_LEVEL);
+
 
     public static void logTLSHandshakeEvent(Instant start,
                                             String peerHost,
@@ -52,8 +67,9 @@ public final class EventHelper {
                                             String cipherSuite,
                                             String protocolVersion,
                                             long peerCertId) {
+        assert securityLogger != null;
         String prepend = getDurationString(start);
-        SECURITY_LOGGER.log(LOG_LEVEL, prepend +
+        securityLogger.log(LOG_LEVEL, prepend +
         " TLSHandshake: {0}:{1,number,#}, {2}, {3}, {4,number,#}",
         peerHost, peerPort, protocolVersion, cipherSuite, peerCertId);
     }
@@ -61,18 +77,18 @@ public final class EventHelper {
     public static void logSecurityPropertyEvent(String key,
                                                 String value) {
 
-        if (isLoggingSecurity()) {
-            SECURITY_LOGGER.log(LOG_LEVEL,
-                "SecurityPropertyModification: key:{0}, value:{1}", key, value);
-        }
+        assert securityLogger != null;
+        securityLogger.log(LOG_LEVEL,
+            "SecurityPropertyModification: key:{0}, value:{1}", key, value);
     }
 
     public static void logX509ValidationEvent(int anchorCertId,
                                          int[] certIds) {
+        assert securityLogger != null;
         String codes = IntStream.of(certIds)
                 .mapToObj(Integer::toString)
                 .collect(Collectors.joining(", "));
-        SECURITY_LOGGER.log(LOG_LEVEL,
+        securityLogger.log(LOG_LEVEL,
                 "ValidationChain: {0,number,#}, {1}", anchorCertId, codes);
     }
 
@@ -85,7 +101,8 @@ public final class EventHelper {
                                                long certId,
                                                long beginDate,
                                                long endDate) {
-        SECURITY_LOGGER.log(LOG_LEVEL, "X509Certificate: Alg:{0}, Serial:{1}" +
+        assert securityLogger != null;
+        securityLogger.log(LOG_LEVEL, "X509Certificate: Alg:{0}, Serial:{1}" +
             ", Subject:{2}, Issuer:{3}, Key type:{4}, Length:{5,number,#}" +
             ", Cert Id:{6,number,#}, Valid from:{7}, Valid until:{8}",
             algId, serialNum, subject, issuer, keyType, length,
@@ -121,6 +138,14 @@ public final class EventHelper {
      * @return boolean indicating whether an event should be logged
      */
     public static boolean isLoggingSecurity() {
-        return LOGGING_SECURITY;
+        // Avoid a bootstrap issue where the commitEvent attempts to
+        // trigger early loading of System Logger but where
+        // the verification process still has JarFiles locked
+        if (securityLogger == null && !JUJA.isInitializing()) {
+            LOGGER_HANDLE.compareAndSet( null, System.getLogger(SECURITY_LOGGER_NAME));
+            loggingSecurity = securityLogger.isLoggable(LOG_LEVEL);
+        }
+        return loggingSecurity;
     }
+
 }

test/jdk/java/util/jar/JarFile/jarVerification/MultiThreadLoad.java
@@ -1,0 +1,128 @@
+/*
+ * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+import jdk.internal.misc.JavaUtilJarAccess;
+import jdk.internal.misc.SharedSecrets;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.jar.JarFile;
+
+public class MultiThreadLoad {
+
+    private static PrintStream out = System.err;
+    static String TEST_CLASS_PATH;
+    private static final JavaUtilJarAccess JUJA = SharedSecrets.javaUtilJarAccess();
+    private static CountDownLatch cdl = new CountDownLatch(1);
+
+    private static <T> Set<T> setOf(Iterable<T> it) {
+        Set<T> s = new HashSet<T>();
+        for (T t : it)
+            s.add(t);
+        return s;
+    }
+
+    private static <T> void checkEquals(Set<T> s1, Set<T> s2, boolean eq) {
+        if (s1.equals(s2) != eq)
+            throw new RuntimeException(String.format("%b %s : %s",
+                                                     eq, s1, s2));
+    }
+
+    abstract static class TestLoader {
+        String name;
+
+        TestLoader(String name) { this.name = name; }
+
+        abstract ServiceLoader<FooService> load();
+    }
+
+    static TestLoader tcclLoader = new TestLoader("Thread context class loader") {
+        ServiceLoader<FooService> load() {
+            return ServiceLoader.load(FooService.class);
+        }
+    };
+
+    static TestLoader systemClLoader = new TestLoader("System class loader") {
+        ServiceLoader<FooService> load() {
+            return ServiceLoader.load(FooService.class, ClassLoader.getSystemClassLoader());
+        }
+    };
+
+    static TestLoader nullClLoader = new TestLoader("null (defer to system class loader)") {
+        ServiceLoader<FooService> load() {
+            return ServiceLoader.load(FooService.class, null);
+        }
+    };
+
+    public static void main(String[] args) {
+        // keep reference to variables for the newly launced process
+        TEST_CLASS_PATH = args[0];
+        for (TestLoader tl : Arrays.asList(tcclLoader, systemClLoader, nullClLoader)) {
+            test(tl);
+        }
+    }
+
+    static void test(TestLoader tl) {
+        Runnable r1 = () -> {
+            ServiceLoader<FooService> sl = tl.load();
+            out.format("%s: %s%n", tl.name, sl);
+
+            // Providers are cached
+            Set<FooService> ps = setOf(sl);
+            cdl.countDown();
+            checkEquals(ps, setOf(sl), true);
+
+            // The cache can be flushed and reloaded
+            sl.reload();
+            checkEquals(ps, setOf(sl), false);
+        };
+
+        Runnable r2 = () -> {
+            jarCrawler(Path.of(TEST_CLASS_PATH));
+        };
+        new Thread(r2).start();
+        new Thread(r1).start();
+
+    }
+
+    private static void jarCrawler(Path p) {
+        try {
+            // let the other thread spin up
+            cdl.await();
+        } catch (InterruptedException e) {
+            // ignore
+        }
+        try {
+            for (int i = MultiProviderTest.NUM_JARS -1; i >= 0; i--) {
+                JUJA.ensureInitialization(new JarFile(TEST_CLASS_PATH + File.separator
+                        + "FooProvider" + i + ".jar"));
+            }
+        } catch (Exception e) {
+            System.out.println("Exception during jar crawl: ");
+            e.printStackTrace(System.out);
+        }
+
+    }
+}